f8e38dbf8f7a422cce989bbf49b847701b313fd7
Follow the wind <36281137+wy414012@users.noreply.github.com>
2024-10-20 13:48:21 +0800

    Update compress_tar.sh

--- compress_tar.sh
+++ compress_tar.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 for file in *.tar; do
-    tar -czf "${file%.tar}.tar.gz" "$file"
+    # 提取文件名，去除.tar 后缀，并去除冒号
+    new_name=$(echo "$file" | sed 's/\.tar$//' | sed 's/:/_/g')
+    tar -czf "${new_name}.tar.gz" "$file"
 done
