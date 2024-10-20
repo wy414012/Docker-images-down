9adcc0f99f7c91982f3a9e2749cd63f4051cc314
Follow the wind <36281137+wy414012@users.noreply.github.com>
2024-10-20 13:52:03 +0800

    添加脚本执行时候的回显

--- compress_tar.sh
+++ compress_tar.sh
@@ -3,5 +3,5 @@
 for file in *.tar; do
     # 提取文件名，去除.tar 后缀，并去除冒号
     new_name=$(echo "$file" | sed 's/\.tar$//' | sed 's/:/_/g')
-    tar -czf "${new_name}.tar.gz" "$file"
+    tar -czvf "${new_name}.tar.gz" "$file"
 done
